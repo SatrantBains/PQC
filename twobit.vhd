fa7752a4ede3dbbb32421a99c037a2da97724838
SatrantBains <123198090+SatrantBains@users.noreply.github.com>
2024-02-25 15:14:16 -0500

    Update twobit.vhd
    signals changed and the semicolon fix

--- twobit.vhd
+++ twobit.vhd
@@ -2,32 +2,27 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity twobit is
-	port(
-		clk : in std_logic;
-		data_in : in std_logic_vector(1 downto 0);
-		data_out : out std_logic_vector(1 downto 0);
-		writeread : in std_logic --1 equals write, 0 equals read	
-		);
-	end twobit;
+    port (
+        clk : in std_logic;
+        writeread : in std_logic; -- 1 equals write, 0 equals read    
+        data_in : in std_logic_vector(1 downto 0);
+        data_out : out std_logic_vector(1 downto 0)
+    );
+end twobit;
 
-architecture Behavioral of twobit is 	
+architecture Behavioral of twobit is
+    signal b : std_logic_vector(1 downto 0) := "00";
+    signal a : std_logic_vector(1 downto 0) := "00";
 begin
-	signal b : std_logic_vector(1 down to 0) := "00";
-   process(clk)
-	 variable a : std_logic_vector(1 down to 0) := "00";
-	begin 
-		if rising_edge(clk) then
-			if writeread = '1' then
-				a := data_in;
-			elsif writeread = '0' then
-				b <= a;
-			end if;
-		end if;
-	end process;
-	data_out <= b;
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            if writeread = '1' then
+                a <= data_in;
+            elsif writeread = '0' then
+                b <= a;
+            end if;
+        end if;
+    end process;
+    data_out <= b;
 end architecture;
-		
-				
-		
-			
-			
