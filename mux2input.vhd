962f237db0d5e723cf479ab492ea3d37c307d01a
kdetlet <130595981+kdetlet@users.noreply.github.com>
2024-03-14 13:57:45 -0400

    2 Input MUX

---
+++ mux2input.vhd
@@ -0,0 +1,25 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity mux2input is
+port (
+	input0 : in std_logic_vector(1 downto 0);
+	input1 : in std_logic_vector(1 downto 0);
+	sig : in std_logic;
+	muxoutput : out std_logic_vector(1 downto 0)
+	);
+end mux2input;
+
+architecture behavioral of mux2input is
+begin
+
+	process(input0, input1, sig)
+	begin 
+		if sig = '0' then
+			muxoutput <= input0;
+		else
+			muxoutput <= input1;
+		end if;
+	end process;
+end behavioral;
+
